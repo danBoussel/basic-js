2cf55f5c04a9ed0572cd18c5f1d72d1a797431b7
Daniil Boussel <daniil7142@gmail.com>
2021-03-29 19:25:06 -0400

    add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,11 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater( str, options ) {
+module.exports = function repeater(str, options) {
 
+    let out = ''
+    if (options.addition !== undefined) {
+        for (i = 1; i < options.additionRepeatTimes; i++) {
+            out += options.addition;
+            if (options.additionSeparator !== undefined) {
+                out += options.additionSeparator;
+            } else out += options.additionSeparator + '|'
+        }
+        out += options.addition;
+    }
+    let = outStr = ''
+    let = finalStr = ''
+    outStr = str + out
+
+    for (i = 1; i < options.repeatTimes; i++) {
+        finalStr += outStr;
+        if (options.separator !== undefined) {
+            finalStr += options.separator
+        } else finalStr = finalStr + '+'
+
+    }
+    finalStr += outStr;
+    return finalStr;
 };
-  
-
-
-
-
-(repeater('REPEATABLE_STRING', {repeatTimes: 2, separator: '222', addition: 'ADDITION', additionRepeatTimes: 3}),
